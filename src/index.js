3e2f9dca00a500b24d3b1fd236d8a7f12b93cf2f
Ratofante <gwilkens.r@gmail.com>
2022-02-17 22:00:43 -0300

    tailwind, react-icons, algo del navbar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//CSS
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//ICONS
+import { FaConnectdevelop } from 'react-icons/fa';
+import { BsCart2 } from 'react-icons/bs';
+
+const NavBar = () => {
+    const title = 'React Store';
+    return (
+        <nav className='navbar flex flex-row drop-shadow-lg shadow-black p-4 border-2 border-solid border-amber-200 rounded-b-lg bg-black'>
+            <div className='text-white flex'>
+                <FaConnectdevelop className='my-auto w-12 h-12' />
+            </div>
+            <h4 className="text-3xl text-amber-200 font-bold ml-4 self-center">{title}</h4>
+            <NavList />
+            <div className='text-white flex'>
+                <BsCart2 className='w-8 h-8  my-auto' />
+            </div>
+        </nav>
+    );
+}
+
+const NavList = () => {
+    return <div className="flex mx-auto">
+        <ul className="flex flex-col xs:flex-row  my-auto">
+            <li className="nav-item">
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#cat">
+                    Categories
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#cat">
+                    Latest
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#cat">
+                    Contact
+                </a>
+            </li>
+        </ul>
+    </div>
+}
+reactDom.render(<NavBar />, document.getElementById('root'));
